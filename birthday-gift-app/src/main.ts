26d2b02e2d67a1b6a4f21957308b71f1b534a931
Mariyem Hidass <hidassmeryam@gmail.com>
2024-05-16 14:37:40 +0200

    Revert "feat: add manifest.json"
    This reverts commit 72d497119abba6251ac04dee387697b3c5121cc2.

--- birthday-gift-app/src/main.ts
+++ birthday-gift-app/src/main.ts
@@ -1,20 +1,7 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
-import { enableProdMode } from '@angular/core';
-import { environment } from './environments/environments.development';
 
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
-
-  
-if (environment.production) {
-  enableProdMode();
-}
-
-
-// Register the service worker
-if ('serviceWorker' in navigator && environment.production) {
-  navigator.serviceWorker.register('/ngsw-worker.js');
-}
