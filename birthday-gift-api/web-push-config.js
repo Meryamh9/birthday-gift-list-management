7eca6e9e8bd1f929f0da24a88a3606c2ab52e13a
Mariyem Hidass <hidassmeryam@gmail.com>
2024-05-24 12:02:26 +0200

    feat: Generating a VAPID key

---
+++ birthday-gift-api/web-push-config.js
@@ -0,0 +1,6 @@
+// web-push-config.js
+module.exports = {
+    publicKey: 'BF5R-Rg59K8rUXM5LEA-0w4ErHoucBZGUyuLTwza5yfcUB0j81FtVVgF1t5C56nA865io-r6E5RxHCEtRUFhu-Y',
+    privateKey: 'Afbhc_Jm9oL8O_iXdnhuSC1TgSzpBs5UyhWBDgMkL1s'
+  };
+  
